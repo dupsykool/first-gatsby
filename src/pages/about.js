ead11e88e266eaf68fd4a7f22b9ba2dfbc536e62
Modupe Aladeojebi <modupealadeojebi@gmail.com>
2021-07-03 21:50:14 +0100

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+//step 1
+import * as React from 'react'
+import Layout from '../components/layout'
+//step 2
+const AboutPage = () => {
+    return(
+        <Layout pageTitle="About Dupsykool">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
+    )
+}
+//step 3
+export default AboutPage
